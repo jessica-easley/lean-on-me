8029f8746e8a840dd592a680c429483a76ffb45a
Easley <jessicaeasley@Jessicas-Air.attlocal.net>
2022-11-08 21:45:31 -0600

    fixed uncaught reference error: google not defined, got nearby hospital & health type places to appear on maps based on proximity

--- script.js
+++ script.js
@@ -1,50 +1,99 @@
 let map;
-const currentPosition = { lat: 32.8203525, lng: -97.0117411 };
+let infoWindow;
+let currentPosition = { lat: 32.8203525, lng: -97.0117411 };
 
-function addMarker(position) {
-    const marker = new google.maps.Marker({
-        position: position,
-        map: map,
+function addMarker(position, type) {
+    console.log("ICON OPTIONS:", google.maps.SymbolPath)
+  let iconUrl = type === 'home' 
+    ? "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+    : "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+
+  const marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    // icon: {
+    //     // url: iconUrl
+    // }
+  });
+
+  return marker;
+}
+
+function addPlaceMarker(place) {
+    console.log("LOCATION", place.geometry.location);
+    const marker = addMarker(place.geometry.location);
+
+    google.maps.event.addListener(marker, "click", function () {
+        infoWindow.setContent(place.name);
+        infoWindow.open(map, this);
     });
-
-    marker.setMap(map);
 }
 
 function initMap() {
   geoFindMe();
-  
+
+  currentPosition = new google.maps.LatLng(32.8203525, -97.0117411);
+
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: currentPosition,
   });
 }
 
-
-
 function geoFindMe() {
   function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
-    currentPosition.lat = latitude; 
-    currentPosition.lng = longitude;
-
-    addMarker(currentPosition);
+    currentPosition = new google.maps.LatLng(latitude, longitude);
+    addMarker(currentPosition, 'home');
     map.panTo(currentPosition);
     console.log("POSITION:", currentPosition);
+
+    // get search results for current area (geolocation)
+    var request = {
+        location: currentPosition,
+        radius: 200,
+        types: ["hospital", "health"], // this is where you set the map to get the hospitals and health related places
+      };
+    
+      infoWindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      console.log('PLACES SERVICE', service);
+    //   service.nearbySearch(request, callback);
+      service.textSearch(request, (results, status) => {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            // get closest (results are ordered by proximity)
+            // addPlaceMarker(results[0]);
+            for (var i = 0; i < results.length; i++) {
+                addPlaceMarker(results[i]);
+                console.log("RESULT:", results[i]);
+            }
+          }
+      })
   }
 
   function error() {
-   console.log("Unable to retrieve your location");
-   addMarker(currentPosition);
+    console.log("Unable to retrieve your location");
+    addMarker(currentPosition);
   }
 
   if (!navigator.geolocation) {
-   console.log("Geolocation is not supported by your browser");
+    console.log("Geolocation is not supported by your browser");
   } else {
-   console.log("Locating…");
+    console.log("Locating…");
     navigator.geolocation.getCurrentPosition(success, error);
   }
 }
 
+// function callback(results, status) {
+//   console.log('PLACES API CALLBACK', status);
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//         addMarker(results[i]);
+//     }
+//   }
+// }
+
+// google.maps.event.addDomListener(window, "load", initialize);
 window.initMap = initMap;
